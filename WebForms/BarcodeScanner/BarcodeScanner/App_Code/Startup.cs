a48f18ed31ab5e0bc9a6e8654b6e602f5e1223fa
alexdotwells <ajwells99@gmail.com>
2018-04-30 21:52:04 -0500

    WebForms initial commit

---
+++ WebForms/BarcodeScanner/BarcodeScanner/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BarcodeScanner.Startup))]
+namespace BarcodeScanner
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
